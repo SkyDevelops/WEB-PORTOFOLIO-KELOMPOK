32f4e155b53fbf2e376bf4293d33723923e9cd47
SkyDevelops <062023107712@mhs.itats.ac.id>
2026-05-14 10:58:34 +0700

    chore: setup github actions for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export", // Diperlukan untuk GitHub Pages
+  images: {
+    unoptimized: true, // Diperlukan karena static export tidak mendukung optimasi image bawaan
+  },
+  // Hapus komentar di bawah ini jika Anda TIDAK menggunakan custom domain
+  // basePath: "/WEB-PORTOFOLIO-KELOMPOK", 
 };
 
 export default nextConfig;
